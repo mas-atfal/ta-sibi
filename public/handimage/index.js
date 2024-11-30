c276c9f3643935b14853ccaf42ae98815b234451
Oskop <oskop17@gmail.com>
2024-11-30 16:39:07 +0700

    handimage for draw handpose

---
+++ public/handimage/index.js
@@ -0,0 +1,189 @@
+const A_sign = "./A.svg"
+const B_sign = "./B.svg"
+const C_sign = "./C.svg"
+const D_sign = "./D.svg"
+const E_sign = "./E.svg"
+const F_sign = "./F.svg"
+const G_sign = "./G.svg"
+const H_sign = "./H.svg"
+const I_sign = "./I.svg"
+const J_sign = "./J.svg"
+const K_sign = "./K.svg"
+const L_sign = "./L.svg"
+const M_sign = "./M.svg"
+const N_sign = "./N.svg"
+const O_sign = "./O.svg"
+const P_sign = "./P.svg"
+const Q_sign = "./Q.svg"
+const R_sign = "./R.svg"
+const S_sign = "./S.svg"
+const T_sign = "./T.svg"
+const U_sign = "./U.svg"
+const V_sign = "./V.svg"
+const W_sign = "./W.svg"
+const X_sign = "./X.svg"
+const Y_sign = "./Y.svg"
+const Z_sign = "./Z.svg"
+
+const A_hand = "./Ahand.svg"
+const B_hand = "./Bhand.svg"
+const C_hand = "./Chand.svg"
+const D_hand = "./Dhand.svg"
+const E_hand = "./Ehand.svg"
+const F_hand = "./Fhand.svg"
+const G_hand = "./Ghand.svg"
+const H_hand = "./Hhand.svg"
+const I_hand = "./Ihand.svg"
+const J_hand = "./Jhand.svg"
+const K_hand = "./Khand.svg"
+const L_hand = "./Lhand.svg"
+const M_hand = "./Mhand.svg"
+const N_hand = "./Nhand.svg"
+const O_hand = "./Ohand.svg"
+const P_hand = "./Phand.svg"
+const Q_hand = "./Qhand.svg"
+const R_hand = "./Rhand.svg"
+const S_hand = "./Shand.svg"
+const T_hand = "./Thand.svg"
+const U_hand = "./Uhand.svg"
+const V_hand = "./Vhand.svg"
+const W_hand = "./Whand.svg"
+const X_hand = "./Xhand.svg"
+const Y_hand = "./Yhand.svg"
+const Z_hand = "./Zhand.svg"
+
+export const Signimage = {
+  A: A_sign,
+  B: B_sign,
+  C: C_sign,
+  D: D_sign,
+  E: E_sign,
+  F: F_sign,
+  G: G_sign,
+  H: H_sign,
+  I: I_sign,
+  J: J_sign,
+  K: K_sign,
+  L: L_sign,
+  M: M_sign,
+  N: N_sign,
+  O: O_sign,
+  P: P_sign,
+  Q: Q_sign,
+  R: R_sign,
+  S: S_sign,
+  T: T_sign,
+  U: U_sign,
+  V: V_sign,
+  W: W_sign,
+  X: X_sign,
+  Y: Y_sign,
+  Z: Z_sign,
+}
+
+export const Signpass = [
+  {
+    src: A_hand,
+    alt: "A",
+  },
+  {
+    src: B_hand,
+    alt: "B",
+  },
+  {
+    src: C_hand,
+    alt: "C",
+  },
+  {
+    src: D_hand,
+    alt: "D",
+  },
+  {
+    src: E_hand,
+    alt: "E",
+  },
+  {
+    src: F_hand,
+    alt: "F",
+  },
+  {
+    src: G_hand,
+    alt: "G",
+  },
+  {
+    src: H_hand,
+    alt: "H",
+  },
+  {
+    src: I_hand,
+    alt: "I",
+  },
+  {
+    src: J_hand,
+    alt: "J",
+  },
+  {
+    src: K_hand,
+    alt: "K",
+  },
+  {
+    src: L_hand,
+    alt: "L",
+  },
+  {
+    src: M_hand,
+    alt: "M",
+  },
+  {
+    src: N_hand,
+    alt: "N",
+  },
+  {
+    src: O_hand,
+    alt: "O",
+  },
+  {
+    src: P_hand,
+    alt: "P",
+  },
+  {
+    src: Q_hand,
+    alt: "Q",
+  },
+  {
+    src: R_hand,
+    alt: "R",
+  },
+  {
+    src: S_hand,
+    alt: "S",
+  },
+  {
+    src: T_hand,
+    alt: "T",
+  },
+  {
+    src: U_hand,
+    alt: "U",
+  },
+  {
+    src: V_hand,
+    alt: "V",
+  },
+  {
+    src: W_hand,
+    alt: "W",
+  },
+  {
+    src: X_hand,
+    alt: "X",
+  },
+  {
+    src: Y_hand,
+    alt: "Y",
+  },
+  {
+    src: Z_hand,
+    alt: "Z",
+  },
+]
